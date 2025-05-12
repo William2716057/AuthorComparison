6307162218043f5cabd557f722710cdb03788309
William <willspri@gmail.com>
2025-05-12 16:31:41 +1200

    update

--- AuthorComparison.py
+++ AuthorComparison.py
@@ -2,3 +2,7 @@
 #use various algorithms to get fingerprints of each text
 #use matplotlib to display similarities visually
 #let user interpret results 
+
+#average sentence length
+#punctuation style
+#word sequence matches

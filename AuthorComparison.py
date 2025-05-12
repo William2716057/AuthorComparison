7e75f429272400e771d6310ac75189c4817486d7
William <willspri@gmail.com>
2025-05-12 16:38:21 +1200

    update

--- AuthorComparison.py
+++ AuthorComparison.py
@@ -1,3 +1,28 @@
+import matplotlib as plt
+
+region_dict = {
+    "color": "colour",
+    "flavor": "flavour",
+    "honor": "honour",
+    "organize": "organise",
+    "realize": "realise",
+    "analyze": "analyse",
+    "center": "centre",
+    "meter": "metre",
+    "theater": "theatre",
+    "defense": "defence",
+    "offense": "offence",
+    "traveling": "travelling",
+    "canceled": "cancelled",
+    "modeling": "modelling",
+    "labeled": "labelled",
+    "check": "cheque",  
+    "gray": "grey",
+    "plow": "plough",
+    "aluminum": "aluminium",
+    "program": "programme",  
+}
+
 #add two text samples
 #use various algorithms to get fingerprints of each text
 #use matplotlib to display similarities visually
@@ -6,3 +31,5 @@
 #average sentence length
 #punctuation style
 #word sequence matches
+#identify region
+#spellcheck
